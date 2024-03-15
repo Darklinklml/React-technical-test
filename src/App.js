84f8985a6109196f84f19146062759f5679196c0
Gerson Perez <gpc9815@gmail.com>
2024-03-14 23:29:52 -0600

    Goals Achived - Added Test Text in location - Added Blue Box in the corner - Added Bottom squares - Added Input with funcionality to init counter - Added button to apply init count - Added button to increment count - Added count display
    Issues
    - Input and label layout incompleted
    - Buttons style incompleted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import {createRef, useState} from "react";
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [contador, setContador] = useState(0)
+
+    const [inputValue, setInputValue] = useState(0)
+
+    function initContador(){
+        setContador(inputValue)
+    }
+
+    function increment (){
+        setContador(contador + 1)
+    }
+
+    function onHandledInputChange(e){
+        setInputValue(parseInt(e.target.value))
+    }
+
+    return (
+        <div className="App-Container">
+            <div className="Top-Container">
+                <h1 className="Text-Test-Box">
+                    Test text
+                </h1>
+
+                <div className="Counter-Box">{contador}</div>
+            </div>
+            <div className="Square-Container">
+                <div className="Counter-Square">
+                        <div className={"Counter-Square-Title"}>Counter Start</div><br/>
+                        <input
+                            className={"rounded-border"}
+                            id={"contador-inicial-input"}
+                            value={inputValue}
+                            onChange={onHandledInputChange}
+                            type={"number"}/>
+
+                </div>
+                <div className="Set-Square">
+                <button className={"Set-Button rounded-border"} onClick={()=>initContador()}>
+                        Set
+                    </button>
+                </div>
+                <div className="Plus-Square">
+                    <button className={"Plus-Button rounded-border"} onClick={()=> increment()} >+</button>
+                </div>
+            </div>
+            <div className={"Blue-Box"}></div>
+        </div>
+    );
 }
 
 export default App;
